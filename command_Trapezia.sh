b489d839fbbeaee242c0a199a4b75e0386594c57
Kenzie Pollard <49292232+kenziepollard@users.noreply.github.com>
2024-12-17 12:30:15 -0800

    Add files via upload

---
+++ command_Trapezia.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -cwd
+#$ -V              
+NUMPOPS=1
+for((POP=1; POP<=$NUMPOPS; POP++))
+	do
+	NUMREPS=10
+	for((REP=1; REP<=$NUMREPS; REP++))
+		do
+		./structure.exe -K $POP -o output_k$POP.Trapezia$REP -D $RANDOM
+		done
+	done
+	
